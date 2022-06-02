fb3c220711e35e5d9fc1c87da718d6b3103217db
marijan <marijan@todah.hr>
2022-06-02 10:26:55 +0200

    Initial commit

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/marijanreactexpo/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lio/codemagic/marijan/reactexpo/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/marijanreactexpo/newarchitecture/components/MainComponentsRegistry;";
+      "Lio/codemagic/marijan/reactexpo/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
